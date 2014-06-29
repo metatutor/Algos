9cbfda74b3150ece321f57040bfae6a4128ac487
Edward Lee <theelee13@gmail.com>
2014-06-29 16:57:50 -0400

    eliminated clutter

--- client/pages/algo/algo.js
+++ client/pages/algo/algo.js
@@ -1,6 +1,5 @@
 Template.algopedia.events = {
 	'submit': function(event,template){
-		event.preventDefault();
 		var name = template.find("input[name=algoName]");
 		var aid = template.find("input[name=algoID]");
 		var desc = template.find("textarea[name=description]");
