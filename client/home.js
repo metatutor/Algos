abec15a1ef7283ce34c5fc63c575f50ef823a03d
Edward Lee <theelee13@gmail.com>
2014-06-30 23:24:23 -0400

    functioning all-search

--- client/home.js
+++ client/home.js
@@ -1,23 +1,18 @@
 Meteor.startup(function(){
-	Session.set("showMoreUser",false);
-	Session.set("userSearch",null);
-	Session.set("algoSearch",null);
+	Session.set("showMore",false);
+	Session.set("searchQuery",null);
 	Session.set("showAmount",5);
-	Session.set("showMoreAlgo",false);
 });
 
 Template.home.events = {
 	'submit': function(event, template){
 		event.preventDefault();
-		Session.set("showMoreUser",true);
-		var usrField = template.find("input[name=userQuery]");
-		Session.set("userSearch",usrField.value);
+		Session.set("showMore",true);
+		var usrField = template.find("input[name=query]");
+		Session.set("searchQuery",usrField.value);
 	},
 	'click button[name=showLessUser]': function(){
-		Session.set("userSearch",null);
-	},
-	'click button[name=showLessAlgo]': function(){
-		Session.set("algoSearch",null);
+		Session.set("searchQuery",null);
 	},
 	'click button[name=show5]': function(){
 		Session.set("showAmount",5);
@@ -28,36 +23,32 @@ Template.home.events = {
 }
 
 Template.home.somethingLookedFor = function(){
-	var userValue = Session.get("userSearch");
-	return !(userValue===null);
+	var value = Session.get("searchQuery");
+	return !(value===null);
 }
 
 Template.home.isValidQuery = function(){
-	var name = Session.get('userSearch');
-	var userInfo = getInfoByName(Meteor.users.find().fetch(),name);
-	return !(userInfo===null);
+	return getAmountResults();
 }
 
 Template.home.getRelevantInfo = function(){
-	var name = Session.get("userSearch");
-	var userInfo = getInfoByName(Meteor.users.find().fetch(),name);
-	var userList = [];
-	userList[0] = userInfo;
-	return userList;
-}
-
-var getInfoByName = function(docs,uname){
-	var i =0;
-	while(i<docs.length){
-		if(docs[i]["username"]===uname){
-			return docs[i];
-		}
-		i++;
-	}
-	return null;
+	var info = Session.get('searchQuery');
+	var userArray = Meteor.users.find({username:info}).fetch();
+	var aidArray = AlgoPedia.find({AiD:info}).fetch();
+	var algoArray = AlgoPedia.find({Name:info}).fetch();
+	return algoArray.concat(aidArray).concat(userArray);
 }
 
 Template.home.newAlgos = function(){
 	var amount = Session.get("showAmount");
 	return AlgoPedia.find( {}, {sort: { When: -1},limit:amount});
+}
+
+var getAmountResults = function(){
+	var info = Session.get('searchQuery');
+	var userSearch = Meteor.users.find({username:info}).count();
+	var aidSearch = AlgoPedia.find({AiD:info}).count();
+	var algoSearch = AlgoPedia.find({Name:info}).count();
+	var entries = userSearch+aidSearch+algoSearch;
+	return entries;
 }
