9248d52be9b3785b98d9117aae8a1a75d7f27b53
Edward Lee <theelee13@gmail.com>
2014-07-01 01:12:42 -0400

    Search now in navbar. Not page-unique.

--- client/home.js
+++ client/home.js
@@ -1,19 +1,8 @@
 Meteor.startup(function(){
-	Session.set("showMore",false);
-	Session.set("searchQuery",null);
 	Session.set("showAmount",5);
 });
 
 Template.home.events = {
-	'submit': function(event, template){
-		event.preventDefault();
-		Session.set("showMore",true);
-		var usrField = template.find("input[name=query]");
-		Session.set("searchQuery",usrField.value);
-	},
-	'click button[name=showLessSearch]': function(){
-		Session.set("searchQuery",null);
-	},
 	'click button[name=show5]': function(){
 		Session.set("showAmount",5);
 	},
@@ -22,48 +11,7 @@ Template.home.events = {
 	}
 }
 
-Template.home.somethingLookedFor = function(){
-	var value = Session.get("searchQuery");
-	return !(value===null);
-}
-
-Template.home.isValidQuery = function(){
-	return getAmountResults();
-}
-
-Template.home.getRelevantInfo = function(){
-	var info = Session.get('searchQuery');
-	var userArray = Meteor.users.find({username:info}).fetch();
-	var aidArray = AlgoPedia.find({AiD:info}).fetch();
-	var algoArray = AlgoPedia.find({Name:info}).fetch();
-	return algoArray.concat(aidArray).concat(userArray);
-}
-
 Template.home.newAlgos = function(){
 	var amount = Session.get("showAmount");
 	return AlgoPedia.find( {}, {sort: { When: -1},limit:amount});
-}
-
-var getAmountResults = function(){
-	var info = Session.get('searchQuery');
-	var userSearch = Meteor.users.find({username:info}).count();
-	var aidSearch = AlgoPedia.find({AiD:info}).count();
-	var algoSearch = AlgoPedia.find({Name:info}).count();
-	var entries = userSearch+aidSearch+algoSearch;
-	return entries;
-}
-
-Template.home.isUser = function(obj){
-	return obj.hasOwnProperty('username');
-}
-
-Template.home.isAlgo = function(obj){
-	return obj.hasOwnProperty('AiD');
-}
-
-Template.home.getName = function(userDoc){
-	if(userDoc.hasOwnProperty('profile')){
-		return userDoc.profile.firstname+' '+userDoc.profile.lastname;
-	}
-	return 'Not yet provided.'
 }
--- client/nav.js
+++ client/nav.js
@@ -2,6 +2,11 @@ Accounts.ui.config(
 		{ passwordSignupFields: 'USERNAME_AND_EMAIL' }
 );
 
+Meteor.startup(function(){
+	Session.set("showMore",false);
+	Session.set("searchQuery",null);
+});
+
 Template.nav.notRegistered = function(){
 	var currentUser = Meteor.user();
 	if(currentUser.hasOwnProperty('emails')){
@@ -16,5 +21,60 @@ Template.nav.notRegistered = function(){
 Template.nav.events = {
 	'click button[name=verify]': function(){
 		Meteor.call("sendEmail",Meteor.userId());
+	},
+	'submit': function(event, template){
+		event.preventDefault();
+		Session.set("showMore",true);
+		var usrField = template.find("input[name=query]");
+		Session.set("searchQuery",usrField.value);
+	},
+	'click button[name=showLessSearch]': function(){
+		Session.set("searchQuery",null);
 	}
+}
+
+Template.nav.somethingLookedFor = function(){
+	var value = Session.get("searchQuery");
+	return !(value===null);
+}
+
+Template.nav.isValidQuery = function(){
+	return getAmountResults();
+}
+
+Template.nav.getRelevantInfo = function(){
+	var info = Session.get('searchQuery');
+	var userArray = Meteor.users.find({username:info}).fetch();
+	var aidArray = AlgoPedia.find({AiD:info}).fetch();
+	var algoArray = AlgoPedia.find({Name:info}).fetch();
+	return algoArray.concat(aidArray).concat(userArray);
+}
+
+Template.nav.newAlgos = function(){
+	var amount = Session.get("showAmount");
+	return AlgoPedia.find( {}, {sort: { When: -1},limit:amount});
+}
+
+var getAmountResults = function(){
+	var info = Session.get('searchQuery');
+	var userSearch = Meteor.users.find({username:info}).count();
+	var aidSearch = AlgoPedia.find({AiD:info}).count();
+	var algoSearch = AlgoPedia.find({Name:info}).count();
+	var entries = userSearch+aidSearch+algoSearch;
+	return entries;
+}
+
+Template.nav.isUser = function(obj){
+	return obj.hasOwnProperty('username');
+}
+
+Template.nav.isAlgo = function(obj){
+	return obj.hasOwnProperty('AiD');
+}
+
+Template.nav.getName = function(userDoc){
+	if(userDoc.hasOwnProperty('profile')){
+		return userDoc.profile.firstname+' '+userDoc.profile.lastname;
+	}
+	return 'Not yet provided.'
 }
