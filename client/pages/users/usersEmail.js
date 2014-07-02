4566c58263c861c9d59dabd186a09fb66750cbae
Edward Lee <theelee13@gmail.com>
2014-07-01 21:30:39 -0400

    fixed user email bug

--- client/pages/users/usersEmail.js
+++ client/pages/users/usersEmail.js
@@ -1,10 +1,10 @@
-Template.algoName.events = {
+Template.getUsersEmail.events = {
 	'click button[name=goTo]': function(){
 		Router.go('pedia', {_id: this.AiD});
 	}  
 }  
 
-Template.userPage.getEmail = function(){
+Template.getUsersEmail.getEmail = function(){
 	var user = this;
 	if(user.hasOwnProperty('emails')){
 		return user.emails[0].address;
