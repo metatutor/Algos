ac5bc29ce10c8325d71be3723740c731ace43d06
William Edward Lee <theelee13@gmail.com>
2014-07-04 14:56:04 -0400

    Delete ga.js

--- client/ga.js
+++
@@ -1,7 +0,0 @@
-Meteor.Loader.loadJs('//www.google-analytics.com/analytics.js',function(){
-   if(!ga){ console.error("Failed to load google analytics"); }
-   // Attach ga to window object
-   //window.ga = ga;
-   ga('create', 'UA-50026049-1', 'navdash.com');
-   ga('send', 'pageview');
-})
