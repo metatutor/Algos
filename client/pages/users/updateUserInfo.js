af9376ab92d356ed52ab4d512e48f9c300448f99
Edward Lee <theelee13@gmail.com>
2014-07-05 00:41:16 -0400

    Fixing GUI for updating user profile

---
+++ client/pages/users/updateUserInfo.js
@@ -0,0 +1,12 @@
+Template.updateUserProfile.events = {
+	'submit': function(event,template){
+		event.preventDefault();
+		var stats = {};
+		var firstname= template.find("input[name=firstname]");
+		var lastname = template.find("input[name=lastname]");
+		stats.firstname = firstname.value;
+		stats.lastname = lastname.value;
+		Meteor.call('updateProfile',stats,Meteor.userId());
+		Session.set("editingProfile",false);
+	}
+}
