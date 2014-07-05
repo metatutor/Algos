b51db0085c1de5a78f6ec071e1b4713ee0048294
Edward Lee <theelee13@gmail.com>
2014-07-05 01:19:50 -0400

    only displays link to user profile on contributions

--- client/smalls/getUserContributions.js
+++ client/smalls/getUserContributions.js
@@ -17,25 +17,3 @@ Template.getUsersContributions.getAlgoContributions = function(){
 	}
 	return cArray;
 }
-
-Template.getUsersContributions.getLangContributions = function(){
-	var i =0;
-	var cArray = [];
-	var lArray = this.profile.languageContributions;
-	while((i<uArray.length)&&(i<5)){
-		cArray.push(Languages.findOne({Name:lArray[lArray.length-i-1]}));
-		i++;
-	}
-	return cArray;
-}
-
-Template.getUsersContributions.getLSIContributions = function(){
-	var i =0;
-	var cArray = [];
-	var lArray = this.profile.lsiContributions;
-	while((i<uArray.length)&&(i<5)){
-		cArray.push(LSIs.findOne({_id:lArray[lArray.length-i-1]}));
-		i++;
-	}
-	return cArray;
-}
