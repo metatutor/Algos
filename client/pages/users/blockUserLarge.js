13498830469c009bf7bafe1f2327e6d6bcb8cdbe
Edward Lee <theelee13@gmail.com>
2014-07-10 01:14:32 -0400

    fixed nav issue crashing algos

--- client/pages/users/blockUserLarge.js
+++ client/pages/users/blockUserLarge.js
@@ -1,12 +1,27 @@
 Template.blockUserLarge.getPoints = function(user){
+	if(user===undefined){
+		return;
+	}
 	return user.profile.points+' Reputation';
 }
 
 Template.blockUserLarge.awards = function(user){
+	if(user===undefined){
+		return;
+	}
 	return _.uniq(user.profile.awards);
 }
 
 Template.blockUserLarge.getPicture = function(user){
-	console.log(user.profile.picture);
+	if(user===undefined){
+		return;
+	}
 	return user.profile.picture;
+}
+
+Template.blockUserLarge.hasAwards = function(user){
+	if(user===undefined){
+		return;
+	}
+	return user.profile.awards.length;
 }
