7eed23a9c82300d432dc06ffa04caf11dd07e7cc
Edward Lee <theelee13@gmail.com>
2014-07-19 21:23:40 -0400

    removed extra user privileges

--- server/logComment.js
+++ server/logComment.js
@@ -1,12 +1,5 @@
 Meteor.methods({
 	logComment:function(obj){
 		Comments.insert(obj);
-	},
-	giveService:function(i,s){
-		ServiceConfiguration.configurations.insert({
-			service: "github",
-			clientId: i,
-			secret: s
-		});
 	}
 });
